837d0b1d4e31993100d8431a02db68b1c8f3fd69
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-07-12 20:41:50 -0400

    Agregados modelo y migracion a la base de datos

--- control_estudios/models.py
+++ control_estudios/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    # los atributos de clase (son las columnas de la tabla)
+    nombre = models.CharField(max_length=64)
+    comision = models.IntegerField()
+
+
+class Estudiante(models.Model):
+    apellido = models.CharField(max_length=256)
+    nombre = models.CharField(max_length=256)
+    email = models.EmailField(blank=True)
+    telefono = models.CharField(max_length=20, blank=True)
+    dni = models.CharField(max_length=32)
+    fecha_nacimiento = models.DateField(null=True)
+
+
+class Profesor(models.Model):
+    apellido = models.CharField(max_length=256)
+    nombre = models.CharField(max_length=256)
+    dni = models.CharField(max_length=32)
+    email = models.EmailField(blank=True)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    profesion = models.CharField(max_length=128)
+    bio = models.TextField(blank=True)
+
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=256)
+    fecha_entrega = models.DateTimeField(auto_now_add=True)
+    esta_aprobado = models.BooleanField(default=False) 
