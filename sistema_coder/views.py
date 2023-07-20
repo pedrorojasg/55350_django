ba4cc00924f93373290ba37942ecea99df898112
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-07-19 20:25:34 -0400

    Agregado template inicio, ahgregado bloque titulo

--- sistema_coder/views.py
+++ sistema_coder/views.py
@@ -21,7 +21,7 @@ def inicio(request):
     contexto = {}
     http_response = render(
         request=request,
-        template_name='base.html',
+        template_name='inicio.html',
         context=contexto,
     )
     return http_response
