ca2351348ce32f7cea5360506e3573415af89c31
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-08-02 19:28:57 -0400

    Add logica user creador

--- control_estudios/models.py
+++ control_estudios/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Curso(models.Model):
     # los atributos de clase (son las columnas de la tabla)
     nombre = models.CharField(max_length=64)
     comision = models.IntegerField()
+    creador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.nombre}, {self.comision}"
@@ -16,6 +19,7 @@ class Estudiante(models.Model):
     telefono = models.CharField(max_length=20, blank=True)
     dni = models.CharField(max_length=32)
     fecha_nacimiento = models.DateField(null=True)
+    creador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.apellido}, {self.nombre}"
@@ -29,6 +33,10 @@ class Profesor(models.Model):
     fecha_nacimiento = models.DateField(null=True, blank=True)
     profesion = models.CharField(max_length=128)
     bio = models.TextField(blank=True)
+    creador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name_plural = "profesores"
 
     def __str__(self):
         return f"{self.apellido}, {self.nombre}"
