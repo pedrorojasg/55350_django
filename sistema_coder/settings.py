591e256761b8c25ae6a5cd877583c11757164244
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-08-02 20:24:28 -0400

    Agregado avatar, configurado archivos media

--- sistema_coder/settings.py
+++ sistema_coder/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +133,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = 'login'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- sistema_coder/urls.py
+++ sistema_coder/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from sistema_coder.views import saludar, saludar_con_fecha, inicio
 
@@ -32,3 +34,5 @@ urlpatterns = [
     # path("saludo/", saludar),
     # path("saludo-hoy/", saludar_con_fecha),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
